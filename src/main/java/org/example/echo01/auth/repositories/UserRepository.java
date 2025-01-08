341ec954c3bf02a3d3394bfa217a10cbb5c63645
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:25:47 +0100

    feat: Add repositories for user and token management

---
+++ src/main/java/org/example/echo01/auth/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.echo01.auth.repositories;
+
+import org.example.echo01.auth.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+} 
