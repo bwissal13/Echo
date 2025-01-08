ecde554396cd0cdf5808f209f1173be362d45a81
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 13:43:03 +0100

    feat: add OTP repository for managing OTP records

---
+++ src/main/java/org/example/echo01/auth/repositories/OTPRepository.java
@@ -0,0 +1,13 @@
+package org.example.echo01.auth.repositories;
+
+import org.example.echo01.auth.entities.OTP;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OTPRepository extends JpaRepository<OTP, Long> {
+    Optional<OTP> findByEmailAndCodeAndUsedFalse(String email, String code);
+    Optional<OTP> findLatestByEmail(String email);
+} 
