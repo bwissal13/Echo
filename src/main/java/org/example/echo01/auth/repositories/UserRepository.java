1da2c3e5d940e5a50ac7a7fb81726729ec80cc99
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 20:26:17 +0100

    feat: Add user search and pagination methods to User Repository

--- src/main/java/org/example/echo01/auth/repositories/UserRepository.java
+++ src/main/java/org/example/echo01/auth/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package org.example.echo01.auth.repositories;
 
 import org.example.echo01.auth.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Page<User> findByFirstnameContainingOrLastnameContainingOrEmailContaining(
+            String firstname, String lastname, String email, Pageable pageable);
 } 
