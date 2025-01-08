66d8b8bf457df5ed41694784ffbca3f9f2191f90
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 17:19:54 +0100

    feat: Add Genre and NotificationType enums for entity support

---
+++ src/main/java/org/example/echo01/auth/enums/Genre.java
@@ -0,0 +1,9 @@
+package org.example.echo01.auth.enums;
+
+public enum Genre {
+    ROMANCE,
+    FANTASY,
+    SCIFI,
+    MYSTERY,
+    THRILLER
+} 
---
+++ src/main/java/org/example/echo01/auth/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package org.example.echo01.auth.enums;
+
+public enum NotificationType {
+    LIKE,
+    COMMENT,
+    FOLLOW
+} 
