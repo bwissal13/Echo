7e268bbddc3566cddb1c3fc5a2f1cdf48128c26d
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:18:09 +0100

    feat: Add RegisterRequest DTO for user registration

---
+++ src/main/java/org/example/echo01/auth/dto/request/RegisterRequest.java
@@ -0,0 +1,33 @@
+package org.example.echo01.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Firstname is required")
+    @Size(min = 2, max = 50, message = "Firstname must be between 2 and 50 characters")
+    private String firstname;
+
+    @NotBlank(message = "Lastname is required")
+    @Size(min = 2, max = 50, message = "Lastname must be between 2 and 50 characters")
+    private String lastname;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String bio;
+} 
