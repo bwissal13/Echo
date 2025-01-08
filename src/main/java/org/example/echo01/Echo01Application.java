c6315291526caf4ff449021e76f76682dc83d9ae
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:21:23 +0100

    feat: Add main application entry point

---
+++ src/main/java/org/example/echo01/Echo01Application.java
@@ -0,0 +1,13 @@
+package org.example.echo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Echo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Echo01Application.class, args);
+    }
+
+}
