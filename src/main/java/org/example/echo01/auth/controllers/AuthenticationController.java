917beb4af3a5b049e6d96f79378518ef81cd3362
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:24:52 +0100

    feat: Add authentication controllers

---
+++ src/main/java/org/example/echo01/auth/controllers/AuthenticationController.java
@@ -0,0 +1,42 @@
+package org.example.echo01.auth.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.example.echo01.auth.dto.request.LoginRequest;
+import org.example.echo01.auth.dto.request.RegisterRequest;
+import org.example.echo01.auth.dto.response.AuthenticationResponse;
+import org.example.echo01.auth.services.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthenticationResponse> refreshToken(
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.refreshToken(request));
+    }
+} 
