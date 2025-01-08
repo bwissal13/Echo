329b0b095f61991d476ab489133713984c27c061
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 20:25:57 +0100

    feat: Add deleteByUser method to OTP Repository

--- src/main/java/org/example/echo01/auth/repositories/OTPRepository.java
+++ src/main/java/org/example/echo01/auth/repositories/OTPRepository.java
@@ -1,6 +1,7 @@
 package org.example.echo01.auth.repositories;
 
 import org.example.echo01.auth.entities.OTP;
+import org.example.echo01.auth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface OTPRepository extends JpaRepository<OTP, Long> {
     Optional<OTP> findByEmailAndCodeAndUsedFalse(String email, String code);
     Optional<OTP> findLatestByEmail(String email);
+    void deleteAllByUser(User user);
 } 
