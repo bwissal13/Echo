f7d3f15ecc76d9b750b2cc12bdd910e7ff4f75f3
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:25:18 +0100

    feat: Add authentication response DTOs

---
+++ src/main/java/org/example/echo01/auth/dto/response/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package org.example.echo01.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String message;
+    private boolean success;
+} 
