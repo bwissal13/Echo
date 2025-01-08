58ebae38283cf3497ac2448393c74e3b1ec201c5
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 11:17:36 +0100

    feat: Add User entity with authentication fields

---
+++ src/main/java/org/example/echo01/auth/entities/User.java
@@ -0,0 +1,71 @@
+package org.example.echo01.auth.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.echo01.auth.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@EqualsAndHashCode(callSuper = false)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String profilePicture;
+
+    @Builder.Default
+    private boolean enabled = true;
+
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+} 
