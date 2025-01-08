0fe40998e7559990cbe61dc7fca4d310eab3371f
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 17:17:58 +0100

    feat: Add Chapter entity with title, content, and book relationship

---
+++ src/main/java/org/example/echo01/auth/entities/Chapter.java
@@ -0,0 +1,23 @@
+package org.example.echo01.auth.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chapters")
+public class Chapter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+} 
