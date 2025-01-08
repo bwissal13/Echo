e931e12fa293435bf606449bdb28525cd0d24a12
bwissal13 <baazizwissal13@gmail.com>
2025-01-08 20:24:21 +0100

    feat: Add RoleChangeRequestRepository for managing role change requests

---
+++ src/main/java/org/example/echo01/auth/repositories/RoleChangeRequestRepository.java
@@ -0,0 +1,16 @@
+package org.example.echo01.auth.repositories;
+
+import org.example.echo01.auth.entities.RoleChangeRequest;
+import org.example.echo01.auth.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoleChangeRequestRepository extends JpaRepository<RoleChangeRequest, Long> {
+    List<RoleChangeRequest> findByUserAndProcessedFalse(User user);
+    Optional<RoleChangeRequest> findByIdAndProcessedFalse(Long id);
+    List<RoleChangeRequest> findAllByProcessedFalse();
+} 
