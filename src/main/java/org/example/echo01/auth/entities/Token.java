d2c6afb71eccb275087b949b1e2db791b843819e
bwissal13 <baazizwissal13@gmail.com>
2025-01-21 15:18:57 +0100

    refactor: update Token entity to use Auditable base class

--- src/main/java/org/example/echo01/auth/entities/Token.java
+++ src/main/java/org/example/echo01/auth/entities/Token.java
@@ -1,17 +1,17 @@
 package org.example.echo01.auth.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.echo01.auth.enums.TokenType;
 import org.example.echo01.common.audit.Auditable;
 
-@Data
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tokens")
-@EqualsAndHashCode(callSuper = false)
 public class Token extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,11 +23,20 @@ public class Token extends Auditable {
     @Enumerated(EnumType.STRING)
     private TokenType tokenType;
 
-    private boolean revoked;
-
     private boolean expired;
+    private boolean revoked;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static Token create(String token, TokenType tokenType, User user) {
+        Token t = new Token();
+        t.setToken(token);
+        t.setTokenType(tokenType);
+        t.setExpired(false);
+        t.setRevoked(false);
+        t.setUser(user);
+        return t;
+    }
 } 
--- src/main/java/org/example/echo01/common/audit/Auditable.java
+++ src/main/java/org/example/echo01/common/audit/Auditable.java
@@ -3,11 +3,12 @@ package org.example.echo01.common.audit;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,10 +16,13 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
-    
+
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -26,12 +30,4 @@ public abstract class Auditable {
     @LastModifiedDate
     @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
-
-    @CreatedBy
-    @Column(name = "created_by", updatable = false)
-    private String createdBy;
-
-    @LastModifiedBy
-    @Column(name = "modified_by")
-    private String modifiedBy;
 } 
