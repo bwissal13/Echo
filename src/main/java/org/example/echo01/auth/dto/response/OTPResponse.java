887a2fe7aa03279cff90616e02d24cc99a8c97fd
bwissal13 <baazizwissal13@gmail.com>
2025-01-21 15:17:26 +0100

    feat: add OTP response DTO

---
+++ src/main/java/org/example/echo01/auth/dto/response/OTPResponse.java
@@ -0,0 +1,20 @@
+package org.example.echo01.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OTPResponse {
+    private Long id;
+    private String email;
+    private LocalDateTime expiryDate;
+    private boolean used;
+    private Long userId;
+} 
